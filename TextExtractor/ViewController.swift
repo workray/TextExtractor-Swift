0fe75cee7ac4295a3c27029bb1dd97d154d411bc
Raymond Ly <workrayz@gmail.com>
2019-03-18 04:11:03 +0000

    initial commit

---
+++ TextExtractor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextExtractor
+//
+//  Created by Mobdev125 on 3/18/19.
+//  Copyright © 2019 Mobdev125. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
